1abbc245baba7910197b47c41b403488a1593b7d
Alberto <albertotaringalvez@gmail.com>
2026-06-21 21:30:13 +0200

    ya funciona el desplegable de submenu aunque se desmonte un poco el css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("Eso funciona si o si");
+
+function showLista(){
+    const elemento = document.getElementsByClassName("listas-menu")[0];
+  
+    console.log("mostra y ocultar el sum-menu")
+
+    if (elemento.style.display === "none" || elemento.style.display === "") {
+        elemento.style.display = "block"; // Se muestra
+    } else {
+        elemento.style.display = "none";  // Se oculta
+    }
+}
